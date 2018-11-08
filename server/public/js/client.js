88192cd97fa27c817d1359fcabc8a1b2585bca77
darrenrector91 <darrenrector91@gmail.com>
2018-11-08 00:13:19 -0600

    getting visitor name w/o 'at' working

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    owAPI = '749da4002db4023d33f77723c728f9ec';
+
+    // Opponent array
+    var opponentArray = [];
+    // console.log('opponentArray', opponentArray);
+
+    $('#schedule').on('click', '.weatherButton', weatherButton);
+
     // Standings
     $.getJSON("./json/standings.json", function (data) {
         displayStandings(data)
@@ -17,13 +25,9 @@ $(document).ready(function () {
             c = parseFloat(standings[i].ties);
 
             totGamesPlayed = a + b + c;
-            // console.log(totGamesPlayed);
             let tiesPct = c * 0.5;
-            // console.log(tiesPct);
             let winTotal = a + tiesPct;
-            // console.log(winTotal);
             let winPct = (winTotal / totGamesPlayed).toFixed(3);
-            console.log(winPct);
 
             let newRow = $('<tr>');
             //appending rows to DOM
@@ -45,34 +49,62 @@ $(document).ready(function () {
     });
 
     function displaySchedule(data) {
-        let schedule = data;
-        // console.log(data);
-        let matchups = schedule.game;
-        // console.log(matchups);
+        let schedule = data.game;
+        // console.log(schedule);
+
+        let matchups = schedule
 
         $('#schedule').empty();
 
         for (let i = 0; i < matchups.length; i++) {
+
+            opp = matchups[i].opponent;
+            // console.log(opp);
+            date = matchups[i].date;
+            result = matchups[i].result;
+            values = [opp, date, result];
+
+            opponentArray.push(opp);
+
             //appending rows to DOM
-            let newRow = $('<tr>');
+            let newRow = $('<tr class="schedule-row">');
             //appending rows to DOM
-            newRow.append('<td>' + matchups[i].date + '</td>');
-            newRow.append('<td>' + matchups[i].opponent + '</td>');
-            newRow.append('<td>' + matchups[i].result + '</td>');
+            newRow.append('<td data-col="date">' + date + '</td>');
+            newRow.append('<td data-col="opponent">' + opp + '</td>');
+            newRow.append('<td data-col="result">' + result + '</td>');
+            newRow.append('<td><button type="button" class="btn btn-primary weatherButton" value=' + date + '><i class="fas fa-cloud-sun"></i>Weather</button><a href="#"></button></td>');
 
             $('#schedule').append(newRow);
         }
     }
 
 
+    //  Script allows for selectable column data when button clicked SEE: html table 'data-col='
+    $('body').on('click', '.weatherButton', function (e) {
+        var col, txt;
+        col = $('#schedule-table').data('col');
+        console.log(col);
+
+        txt = $(this).parent().siblings("td[data-col=" + col + "]").text();
+        e.preventDefault();
+
+        var visitor = txt;
+        visitor = visitor.replace("at ", "")
+        console.log('visitor: ', visitor);
+
+    });
+
+
+    // var str = "data-123";
+    // str = str.replace("data-", "");
 
     // Articles
     $.getJSON("./json/articles.json", function (data) {
-        // console.log(data); 
         displayAPIArticles(data)
     });
 
     function displayAPIArticles(data) {
+
         let articles = data;
         $('#articles').empty();
         for (let i = 0; i < articles.length; i++) {
@@ -80,5 +112,60 @@ $(document).ready(function () {
             // $('.articles').append('<h5>' + articles[i].article + '</h5>');
             $('.articles').append('<ul><li>' + ('<a href="' + articles[i].link + '"target="_blank">' + articles[i].article + '</a>') + '</li></ul>')
         }
+    }
+
+    function weatherButton(value) {
+
+        let opponent = $(this).val();
+        // console.log(opponent);
+
+        if (opponent.includes("at")) {
+            console.log('true');
+
+            getWeather(opponent)
+        }
+        else {
+            // makes home games use 'Chicago' for the weather city because Chicago is the home team
+            opponent = 'Chicago'
+            getWeather(opponent)
+        }
+    }
+
+    // weather done by city name using openweather API
+    // if today's date IS game date give current weather
+    // if today's date is <= 5 days but !current day give 3 day forcast
+    // if today's date is > 5 days from game day, give 16 day forcast
+    // if date is earlier than today's date then do not show weather button
+
+    function getWeather(values) {
+        let city = values;
+        console.log(city);
+
+        let dateObj = new Date();
+        let month = dateObj.getMonth() + 1; //months from 1-12
+        let day = dateObj.getDate();
+        let year = dateObj.getFullYear();
+        let fourDays = dateObj.getDate() + 5;
+
+        todaysDate = month + "/" + day + "/" + year;
+        fourDaysStr = month + "/" + fourDays + "/" + year;
+
+        var params = {
+            type: 'GET',
+            success: function (data) {
+                console.log('weather: ', data);
+            },
+        };
+        if (todaysDate === todaysDate) {
+            params.url = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial';
+        }
+        if (todaysDate > todaysDate && (todaysDate <= fourDaysStr)) {
+            params.url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + ',US&units=imperial&cnt=5&APPID=' + owAPI;
+        }
+        else {
+            params.url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + ',US&units=imperial&cnt=16&APPID=' + owAPI;
+        }
+
+        $.ajax(params);
     }
 })
